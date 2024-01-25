15e50e3dfe861ce134a398ab8d3777c7651af2c2
itxcrusher <muhammadhassaanjaved99@gmail.com>
2024-01-25 22:13:07 +0500

    CRUD Complete

--- frontend/script.js
+++ frontend/script.js
@@ -1,128 +1,110 @@
 console.clear();
 
 // AWS SDK Configuration
-AWS.config.update({
-    region: 'us-east-1',
+AWS.config.update({ region: "us-east-1" });
+//"https://n2wp9qmj5i.execute-api.us-east-1.amazonaws.com/Prod/students"
+var API_URL = 
+  "https://kkwrv5ek42.execute-api.us-east-1.amazonaws.com/Dev/items";
+
+// Get all items
+function fetchData(params) {
+  $.ajax({
+    url: API_URL,
+    method: "GET",
+    dataType: "json",
+    success: function (responseData) {
+      var array = responseData;
+      var tableBody = document.getElementById('tableBody');
+      tableBody.innerHTML = '';
+      array.forEach(function(obj) {
+        var row = tableBody.insertRow();
+        var cell = row.insertCell(0);
+        cell.textContent = obj.id;
+      });
+    },
+    error: function (error) {
+      console.error("AJAX error:", error);
+    },
+  });
+}
+fetchData();
+
+// Get item by ID
+$("#getForm").submit((e) => {
+  e.preventDefault();
+  var itemId = $("#getForm #get-id").val();
+  $.ajax({
+    url: API_URL + "/" + itemId,
+    method: "GET",
+    dataType: "json",
+    success: function (responseData) {
+      $("#display-id").html(JSON.stringify(responseData, null, 2));
+    },
+    error: function (error) {
+      alert("Unsuccessful!");
+    },
+  });
 });
 
-// API Gateway endpoint
-// const apiEndpoint = 'https://n2wp9qmj5i.execute-api.us-east-1.amazonaws.com/Prod';
-
-// async function createData() {
-//     const data = document.getElementById('createData').value;
-//     await sendData('POST', '/students', { data });
-//     document.getElementById('result').innerText = 'Data created successfully.';
-// }
-
-// async function readData() {
-//     const id = document.getElementById('readId').value;
-//     await sendData('GET', `/students/${id}`);
-//     document.getElementById('result').innerText = 'Data read successfully: ' + responseData;
-// }
-
-// async function updateData() {
-//     const id = document.getElementById('updateId').value;
-//     const data = document.getElementById('updateData').value;
-//     await sendData('PUT', `/students/${id}`, { data });
-//     document.getElementById('result').innerText = 'Data updated successfully.';
-// }
-
-// async function deleteData() {
-//     const id = document.getElementById('deleteId').value;
-//     await sendData('DELETE', `/students/${id}`);
-//     document.getElementById('result').innerText = 'Data deleted successfully.';
-// }
-
-// async function sendData(method, path, body) {
-//     try {
-//         const apiGateway = new AWS.ApiGatewayManagementApi({
-//             endpoint: apiEndpoint,
-//         });
-
-//         const connectionId = ''; // connection ID
-
-//         const params = {
-//             ConnectionId: connectionId,
-//             Data: JSON.stringify({
-//                 method,
-//                 path,
-//                 body,
-//             }),
-//         };
-
-//         await apiGateway.postToConnection(params).promise();
-//     } catch (error) {
-//         console.error('Error:', error);
-//         document.getElementById('result').innerText = 'An error occurred: ' + error.message;
-//     }
-// }
-
-
-    // Function to fetch data from DynamoDB
-    //async function fetchData() {
-    //   try {
-    //     // Replace 'YOUR_API_GATEWAY_URL' with the actual URL of your API Gateway
-    //     const apiUrl = "https://n2wp9qmj5i.execute-api.us-east-1.amazonaws.com/Prod/students";
-        
-    //     // Make a GET request to the API Gateway endpoint
-    //     const response = await fetch(apiUrl);
-        
-    //     // Check if the request was successful (status code 200)
-    //     if (response.ok) {
-    //       // Parse the JSON response
-    //       const data = await response.json();
-          
-    //       // Display the data on the webpage
-    //       document.getElementById('data').innerText = JSON.stringify(data, null, 2);
-    //     } else {
-    //       // Handle error cases
-    //       console.error('Failed to fetch data:', response.statusText);
-    //     }
-    //   } catch (error) {
-    //     console.error('Error:', error);
-    //   }
-
-      // Use the fetch function to make a GET request to the API endpoint
-    //     fetch('https://n2wp9qmj5i.execute-api.us-east-1.amazonaws.com/Prod/students')
-    //     .then(function (response) {
-    //     // Check if the request was successful (status 200)
-    //     if (!response.ok) {
-    //         throw new Error('Network response was not ok');
-    //     }
-
-    //     // Parse the JSON response
-    //     return response.json();
-    //     })
-    //     .then(function (responseData) {
-    //     // Do something with the data
-    //     console.log(responseData);
-    //     })
-    //     .catch(function (error) {
-    //     // Handle errors
-    //     console.error('Fetch error:', error);
-    //     });
-
-    // }
-
-    // Call the fetchData function when the page loads
-    //window.onload = fetchData();
-
-
-    // Use jQuery's AJAX method to fetch data from the API endpoint
+// Put Items
+$("#putForm").submit((e) => {
+  e.preventDefault();
+  var formData = {
+    id: $("#putForm #put-id").val(),
+  };
   $.ajax({
-        url: 'https://dog.ceo/api/breeds/list/all',
-        method: 'GET',
-        dataType: 'json', // specify the expected data type
-        success: function (responseData) {
-        // Handle the successful response
-        console.log(responseData);
+    url: API_URL,
+    method: "POST",
+    dataType: "json",
+    contentType: "application/json",
+    data: JSON.stringify(formData),
+    success: function (responseData) {
+      fetchData();
+    },
+    error: function (error) {
+      console.error("AJAX error:", error);
+      alert("Error creating item.");
+    },
+  });
+});
 
-        // Do something with the data
-        // For example, update the DOM with the received data
-        $('#data').html(JSON.stringify(responseData, null, 2));
-        },
-        error: function (error) {
-        // Handle errors
-        console.error('AJAX error:', error);
-        }
-    });
+// Update Items
+$("#updateForm").submit((e) => {
+  e.preventDefault();
+  var itemId = $("#updateForm #update-id").val();
+  var formData = {
+    id: $("#updateForm #new-id").val(),
+  };
+  $.ajax({
+    url: API_URL + "/" + itemId,
+    method: "PATCH",
+    dataType: "json",
+    contentType: "application/json",
+    data: JSON.stringify(formData),
+    success: function (responseData) {
+      fetchData();
+    },
+    error: function (error) {
+      console.error("AJAX error:", error);
+      alert("Error updating item.");
+    },
+  });
+});
+
+// Delete Item by Id
+$("#deleteForm").submit((e) => {
+  e.preventDefault();
+  var itemId = $("#deleteForm #delete-id").val();
+  $.ajax({
+    url: API_URL + "/" + itemId,
+    method: "DELETE",
+    dataType: "json",
+    success: function (responseData) {
+      fetchData();
+    },
+    error: function (error) {
+      console.error("AJAX error:", error);
+      alert("Error deleting item!");
+    },
+  });
+});
--- students/app.py
+++ students/app.py
@@ -10,6 +10,7 @@ table = ddb.Table('students')
 s3 = boto3.client('s3')
 sns = boto3.client('sns')
 
+################## CRUD Method 1
 @app.route('/students', methods=['GET', 'POST'])
 def put_list_students():
     if request.method == 'GET':
@@ -40,6 +41,178 @@ def get_patch_delete_student(id):
 def json_response(data, response_code=200):
     return json.dumps(data), response_code, {'Content-Type': 'application/json'}
 
+################## CRUD Method 2
+    # Get All Items
+def get_all_items():
+    items = table.scan()
+    return items['Items']
+
+def get_all(event, context):
+    try:
+        items = get_all_items()
+        return {
+            'statusCode': 200,
+            'body': json.dumps(items),
+            'headers': {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*'
+            }
+        }
+    except Exception as e:
+        return {
+            'statusCode': 500,
+            'body': f'Error: {str(e)}',
+            'headers': {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*'
+            }
+        }
+    
+    # Get Item by ID
+def get_item_by_id(item_id):
+    item = table.get_item(Key={'id': item_id})
+    return item['Item'] if 'Item' in item else None
+
+def get_id(event, context):
+    try:
+        item_id = event['pathParameters']['id']
+        item = get_item_by_id(item_id)
+
+        if item:
+            return {
+                'statusCode': 200,
+                'body': json.dumps(item),
+                'headers': {
+                    'Content-Type': 'application/json',
+                    'Access-Control-Allow-Origin': '*'
+                }
+            }
+        else:
+            return {
+                'statusCode': 404,
+                'body': 'Item not found',
+                'headers': {
+                    'Content-Type': 'application/json',
+                    'Access-Control-Allow-Origin': '*'
+                }
+            }
+    except Exception as e:
+        return {
+            'statusCode': 500,
+            'body': f'Error: {str(e)}',
+            'headers': {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*'
+            }
+        }
+    
+    # Put Items
+def put_item(item_id):   # , item_name
+    response = table.put_item(Item={'id': item_id})     # , 'name': item_name
+    return response
+
+def put_items(event, context):
+    try:
+        data = json.loads(event['body'])
+        item_id = data['id']
+
+        response = put_item(item_id)    # , item_name
+
+        return {
+            'statusCode': 200,
+            'body': json.dumps(response),
+            'headers': {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Methods': '*'
+            }
+        }
+    except Exception as e:
+        return {
+            'statusCode': 500,
+            'body': f'Error: {str(e)}',
+            'headers': {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Methods': '*'
+            }
+        }
+    
+# Update Item by ID
+def update_item(item_id, updated_id):
+    delete_item(item_id)
+    response = put_item(updated_id)
+    # attribute_updates = {key: {'Value': value, 'Action': 'PUT'}
+    #                          for key, value in request.form.items()}
+    # response = table.update_item(Key=item_id, AttributeUpdates=attribute_updates)
+    # response = table.update_item(
+    #     Key={'id': item_id},
+    #     UpdateExpression='SET id = :updated_id',
+    #     #ExpressionAttributeNames={'#id': 'id'},
+    #     ExpressionAttributeValues={':updated_id': updated_id}
+    # )
+    return response
+
+def update_items(event, context):
+    try:
+        data = json.loads(event['body'])
+        updated_id = data['id']
+        item_id = event['pathParameters']['id'] # data['id']
+
+        response = update_item(item_id, updated_id)
+
+        return {
+            'statusCode': 200,
+            'body': json.dumps(response),
+            'headers': {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Methods': '*'
+            }
+        }
+    except Exception as e:
+        return {
+            'statusCode': 500,
+            'body': f'Error: {str(e)}',
+            'headers': {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Methods': '*'
+            }
+        }
+
+# Delete Item by ID
+def delete_item(item_id):
+    response = table.delete_item(Key={'id': item_id})
+    return response
+
+def delete_items(event, context):
+    try:
+        item_id = event['pathParameters']['id']
+
+        response = delete_item(item_id)
+
+        return {
+            'statusCode': 200,
+            'body': json.dumps(response),
+            'headers': {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Methods': '*'
+            }
+        }
+    except Exception as e:
+        return {
+            'statusCode': 500,
+            'body': f'Error: {str(e)}',
+            'headers': {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Methods': '*'
+            }
+        }
+    
+################# S3 Lambda Function to push notifications
 def notification_handler(event, context):
     # Get the uploaded object details from the event
     bucket = event['Records'][0]['s3']['bucket']['name']
